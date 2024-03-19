32f0e2ee4330db157dd73596b25df0d4657264c3
Maxim0Mukhamedov <m2204642@edu.misis.ru>
2024-03-19 18:38:43 +0300

    feat: add visualisation with ImPlot

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -9,14 +10,102 @@
 #include <opencv2/highgui.hpp>
 
 #include <implot.h>
-#include <imgui.h>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
+#ifdef __EMSCRIPTEN__
+#include "../libs/emscripten/emscripten_mainloop_stub.h"
+#endif
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+
+
+// Simple helper function to load an image into a OpenGL texture with common settings
+bool LoadTextureFromFile(const char* filename, GLuint* out_texture, int* out_width, int* out_height)
+{
+    // Load from file
+    int image_width = 0;
+    int image_height = 0;
+    unsigned char* image_data = stbi_load(filename, &image_width, &image_height, NULL, 4);
+    if (image_data == NULL)
+        return false;
+
+    // Create a OpenGL texture identifier
+    GLuint image_texture;
+    glGenTextures(1, &image_texture);
+    glBindTexture(GL_TEXTURE_2D, image_texture);
+
+    // Setup filtering parameters for display
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
+
+    // Upload pixels into texture
+#if defined(GL_UNPACK_ROW_LENGTH) && !defined(__EMSCRIPTEN__)
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+#endif
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+    stbi_image_free(image_data);
+
+    *out_texture = image_texture;
+    *out_width = image_width;
+    *out_height = image_height;
+
+    return true;
+}
+
+
+
+// PSF CODE
 const int cols = 20;
 const int rows = 20;
 const int colWidth = 10;
 const int rowWidth = 10;
 const int xStart = 0;
 const int yStart = 0;
+
+class Points {
+private:
+    std::vector<double> x;
+    std::vector<double> y;
+public:
+    void push_back(std::pair<double,double> point) {
+        x.push_back(point.first);
+        y.push_back(point.second);
+    }
+    std::vector<double> getAbscisses() {
+        return x;
+    }
+    std::vector<double> getOrdinates() {
+        return y;
+    }
+    int size() {
+        return x.size();
+    }
+};
 
 cv::Mat loadImage(std::string relative_path) {
     std::string path = cv::samples::findFile(relative_path);
@@ -50,7 +139,7 @@ std::vector<std::vector<double>> calculateSectionsIntense(const cv::Mat &roiImag
 
 int findMeanElementIndex(const std::vector<double>& rowSectionIntenses, const double& avgIntense) {
     return  static_cast<int>(std::upper_bound(rowSectionIntenses.begin(), rowSectionIntenses.end(), avgIntense) -
-            rowSectionIntenses.begin());
+                             rowSectionIntenses.begin());
 }
 
 std::vector<int> findSectionBorderIndex(const std::vector<std::vector<double>> &sectionIntense) {
@@ -74,7 +163,7 @@ std::vector<std::vector<double>> calculateESFabscisses(const std::vector<int> &b
     std::vector<std::vector<double>> esfPointOrdinate(rows, std::vector<double>(cols));
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            esfPointOrdinate[i][j] = (j - borderIndex[i]) * cosPlane;
+            esfPointOrdinate[i][j] = (j - borderIndex[i]) * (cosPlane + cosPlane/(rows*cols)*std::pow(-1,i) * i );
         }
     }
     return esfPointOrdinate;
@@ -83,9 +172,9 @@ std::vector<std::vector<double>> calculateESFabscisses(const std::vector<int> &b
 std::vector<std::pair<double, double>> averageByAbscisse(std::vector<std::pair<double, double>> points) {
     std::sort(points.begin(),points.end());
     std::vector<std::pair<double,double>> averagePoints;
-    double cnt = 0;
-    double sum = 0;
-    for (int i = 0; i < cols * rows - 1; i ++) {
+    double cnt = 1;
+    double sum = points[0].second;
+    for (int i = 0; i < points.size() - 1; i ++) {
         if (points[i + 1].first == points[i].first) {
             if (cnt == 0) {
                 cnt = 2;
@@ -96,14 +185,57 @@ std::vector<std::pair<double, double>> averageByAbscisse(std::vector<std::pair<d
             sum += points[i + 1].second;
         } else {
             averagePoints.push_back({points[i].first,sum/cnt});
-            cnt = 0;
-            sum = 0;
+            cnt = 1;
+            sum = points[i + 1].second;
         }
     }
     return averagePoints;
 }
 
-std::vector<std::pair<double,double>> calculateESF(const cv::Mat& roiImage) {
+double biSquare(double x) {
+    if (-1 <= x && x <= 1) {
+        return (1-x*x)*(1-x*x);
+    } else {
+        return 0;
+    }
+}
+
+std::vector<std::pair<double, double>> calculateWindow(const std::vector<std::pair<double, double>>& ESF, double x, double window) {
+    std::vector<std::pair<double, double>> wp;
+    for (int i = 0; i < ESF.size(); i++) {
+        if ( (x - window) < ESF[i].first && ESF[i].first < (x + window)) {
+            wp.push_back(ESF[i]);
+        } else if (ESF[i].first > x + window){
+            break;
+        }
+    }
+    return wp;
+}
+
+double calculateSumWeight(const std::vector<std::pair<double, double>>& point, double x, double window) {
+    double sum = 0;
+    for (int i = 0; i < point.size(); i++) {
+        sum += biSquare( (point[i].first - x)/window);
+    }
+    return sum;
+}
+
+std::vector<std::pair<double, double>> linearSmoothing(const std::vector<std::pair<double, double>>& ESF, double step) {
+    double window = 2;
+    std::vector<std::pair<double, double>> smoothedESF;
+    for (double x = ESF[0].first; x < ESF[ESF.size() - 1].first; x += step) {
+        std::vector<std::pair<double, double>> pointsInWindow = calculateWindow(ESF, x, window);
+        double Wj = calculateSumWeight(pointsInWindow,x,window);
+        double sum = 0;
+        for (int i = 0; i < pointsInWindow.size(); i++) {
+            sum += biSquare((pointsInWindow[i].first - x) / window)/Wj * pointsInWindow[i].second;
+        }
+        smoothedESF.push_back({x,sum});
+    }
+    return smoothedESF;
+}
+
+Points calculateESF(const cv::Mat& roiImage) {
     std::vector<std::vector<double>> sectionIntense = calculateSectionsIntense(roiImage);
     std::vector<int> borderIndex = findSectionBorderIndex(sectionIntense);
     double cosPlane = calculatePlaneCos(borderIndex);
@@ -115,26 +247,232 @@ std::vector<std::pair<double,double>> calculateESF(const cv::Mat& roiImage) {
         }
     }
     ESF = averageByAbscisse(ESF);
-    return ESF;
+    ESF = linearSmoothing(ESF,0.1);
+    Points esf;
+    for (auto i : ESF) {
+        std::cout << i.first << ' ' << i.second << std::endl;
+        esf.push_back(i);
+    }
+    return esf;
 }
 
 
-std::vector<std::pair<double, double>> calculateLSF(const cv::Mat& roiImage) {
-    std::vector<std::pair<double, double>> ESF = calculateESF(roiImage);
-    std::vector<std::pair<double, double>>& LSF = ESF;
+Points calculateLSF(const cv::Mat& roiImage) {
+    Points ESF = calculateESF(roiImage);
+    Points LSF;
     for (int i = 0; i < ESF.size() - 1; i ++) {
-        LSF[i] = {ESF[i].first, (ESF[i + 1].second - ESF[i].second)/(ESF[i+1].first - ESF[i].first)};
+        LSF.push_back({ESF.getAbscisses()[i], (ESF.getOrdinates()[i + 1] - ESF.getOrdinates()[i])/(ESF.getAbscisses()[i+1] - ESF.getAbscisses()[i])});
     }
-    LSF[ESF.size() - 1] = {ESF[ESF.size() - 1].first, ESF[ESF.size() - 2].second};
+    LSF.push_back({LSF.getAbscisses()[ESF.size() - 2],LSF.getOrdinates()[ESF.size() - 2]});
     return LSF;
 }
 
-int main() {
+
+
+// Main code
+int main(int, char**)
+{
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100
+    const char* glsl_version = "#version 100";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImPlot::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsLight();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+#ifdef __EMSCRIPTEN__
+    ImGui_ImplGlfw_InstallEmscriptenCanvasResizeCallback("#canvas");
+#endif
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    // - Our Emscripten build process allows embedding fonts to be accessible at runtime from the "fonts/" folder. See Makefile.emscripten for details.
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != nullptr);
+
+    // Our state
+    bool show_demo_window = false;
+    bool show_another_window = true;
+    bool show_plot_window = true;
+    bool show_example_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     cv::Mat img = loadImage("/home/maxim/CLionProjects/psfEstimate/testData/synthEdgeImage.png");
     cv::Mat roiImage = splitImageToSections(img);
-    std::vector<std::pair<double, double>> ESF = calculateESF(roiImage);
-    std::vector<std::pair<double, double>> LSF = calculateLSF(roiImage);
+    Points ESF = calculateESF(roiImage);
+    Points LSF = calculateLSF(roiImage);
+
+    // Main loop
+#ifdef __EMSCRIPTEN__
+    // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
+    // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
+    io.IniFilename = nullptr;
+    EMSCRIPTEN_MAINLOOP_BEGIN
+#else
+    while (!glfwWindowShouldClose(window))
+#endif
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        if (show_example_window) {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!",&show_example_window);                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+
+            ImGui::End();
+
+
+
+        }
+
+        int my_image_width = 0;
+        int my_image_height = 0;
+        GLuint my_image_texture = 0;
+        bool ret = LoadTextureFromFile("/home/maxim/CLionProjects/psfEstimate/testData/synthEdgeImage.png", &my_image_texture, &my_image_width, &my_image_height);
+        IM_ASSERT(ret);
+
+        // 2.1 Show simple plot window with ImPlot
+        if (show_plot_window) {
+            ImGui::Begin("Plots", &show_plot_window);
+            if (ImPlot::BeginPlot("ESF")) {
+//                for (int i = 0; i < rows; i++) {
+//                    ImPlot::PlotScatter("ESF PLOT", ESF.getAbscisses().data() + rows * i, ESF.getOrdinates().data() + rows * i,rows);
+//                    ImPlot::PlotLine("ESF PLOT", ESF.getAbscisses().data() + rows * i, ESF.getOrdinates().data() + rows * i,rows);
+//                }
+//                ImPlot::PlotScatter("ESF PLOT", ESF.getAbscisses().data(), ESF.getOrdinates().data(),ESF.size());
+                ImPlot::PlotLine("ESF PLOT", ESF.getAbscisses().data(), ESF.getOrdinates().data(),ESF.size());
+                ImPlot::EndPlot();
+            }
+            if (ImPlot::BeginPlot("LSF")) {
+                ImPlot::PlotLine("LSF PLOT", LSF.getAbscisses().data(), LSF.getOrdinates().data(), LSF.size());
+                ImPlot::EndPlot();
+            }
+
+            if (ImGui::Button("Close"))
+                show_plot_window = false;
+            ImGui::End();
+        }
+
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+             ImGui::Text("pointer = %x", my_image_texture);
+            ImGui::Text("size = %d x %d", my_image_width, my_image_height);
+            ImGui::Image((void*)(intptr_t)my_image_texture, ImVec2(my_image_width, my_image_height));
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            if (ImGui::Button("Show Plot"))
+                show_plot_window = true;
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+#ifdef __EMSCRIPTEN__
+    EMSCRIPTEN_MAINLOOP_END;
+#endif
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImPlot::DestroyContext();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
